d6378c6b5a3563c0308a81a7726a273da7d5ae23
Sunu Ilham Pradika <sunudika@gmail.com>
2019-10-31 15:39:59 +0700

    added function to show grid recycler view layoout

--- app/src/main/java/com/dicoding/picodiploma/surecyclerview/ui/MainActivity.java
+++ app/src/main/java/com/dicoding/picodiploma/surecyclerview/ui/MainActivity.java
@@ -1,6 +1,7 @@
 package com.dicoding.picodiploma.surecyclerview.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -8,6 +9,7 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.dicoding.picodiploma.surecyclerview.adapter.GridHeroAdapter;
 import com.dicoding.picodiploma.surecyclerview.model.Hero;
 import com.dicoding.picodiploma.surecyclerview.utils.HeroesData;
 import com.dicoding.picodiploma.surecyclerview.R;
@@ -38,6 +40,12 @@ public class MainActivity extends AppCompatActivity {
         rvHeroes.setAdapter(listHeroAdapter);
     }
 
+    private void showRecyclerGrid() {
+        rvHeroes.setLayoutManager(new GridLayoutManager(this, 2));
+        GridHeroAdapter gridHeroAdapter = new GridHeroAdapter(list);
+        rvHeroes.setAdapter(gridHeroAdapter);
+    }
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_main, menu);
@@ -53,9 +61,11 @@ public class MainActivity extends AppCompatActivity {
     public void setMode(int selectedMode) {
         switch (selectedMode) {
             case R.id.action_list:
+                showRecyclerList();
                 break;
 
             case R.id.action_grid:
+                showRecyclerGrid();
                 break;
 
             case R.id.action_cardview:
